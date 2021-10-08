4dae470b0faa2ea42f10beb1739070bc54e11a49
sophiagavrila <msophiagavrila@gmail.com>
2021-10-08 09:02:10 -0400

    add url link to photo

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   title = "User Portal"
-  image = 'assets/network.png';
+  image = 'https://freesvg.org/img/black_orange_men_cloud.png'
 
   constructor() { }
 
